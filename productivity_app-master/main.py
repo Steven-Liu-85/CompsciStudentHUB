ad905fd7e1b975076ea6b09d59d15beeb82843b5
Steven-Liu-85 <27ho059@ismanila.org>
2025-05-16 14:18:10 +0800

    front end bug fix1

--- productivity_app-master/main.py
+++ productivity_app-master/main.py
@@ -1,11 +1,6 @@
 from app import app
 
 import sys
-if sys.version_info >= (3, 10):
-    import collections, collections.abc
-    collections.Mapping        = collections.abc.Mapping
-    collections.MutableMapping = collections.abc.MutableMapping
-    collections.Callable       = collections.abc.Callable   # if needed
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5001, debug=True)
